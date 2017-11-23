e1a6d2ec4665ca678169b34db79eb9f4aa8516ba
euro <phuchit@omise.co>
2017-11-23 14:37:20 +0700

    Implemented login page
    Summary: Implement UI for login page
    
    Test Plan: -
    
    Reviewers: thibault, mederic
    
    Reviewed By: thibault, mederic
    
    Maniphest Tasks: T382
    
    Differential Revision: https://phabricator.omisego.io/D231

---
+++ app/src/main/java/co/omisego/omgshop/LoginActivity.kt
@@ -0,0 +1,43 @@
+package co.omisego.omgshop
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        initInstance()
+    }
+
+    private fun initInstance() {
+        btnLogin.setOnClickListener {
+            // TODO : add validation, handle error, navigate to ProductList page
+            Toast.makeText(this@LoginActivity, "Login", Toast.LENGTH_SHORT).show()
+        }
+        val clickSpan = object : ClickableSpan() {
+            override fun onClick(v: View) {
+                // TODO : Go to register page
+                Toast.makeText(this@LoginActivity, "Register", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val ss = SpannableString(getString(R.string.activity_login_register))
+
+        // To make the word "Register" clickable with hi.
+        ss.setSpan(clickSpan, 27, 35, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        tvRegister.text = ss
+        tvRegister.movementMethod = LinkMovementMethod.getInstance()
+
+    }
+}
