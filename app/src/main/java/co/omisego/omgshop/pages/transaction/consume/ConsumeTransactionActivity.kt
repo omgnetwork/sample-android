b813a809147a96f3d352621788c62929ae68f5a6
euro <phuchit@omise.co>
2018-06-23 09:02:36 +0700

    Create generate and consume transaction UI

---
+++ app/src/main/java/co/omisego/omgshop/pages/transaction/consume/ConsumeTransactionActivity.kt
@@ -0,0 +1,13 @@
+package co.omisego.omgshop.pages.transaction.consume
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import co.omisego.omgshop.R
+
+class ConsumeTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consume_transaction)
+    }
+}
---
+++ app/src/main/java/co/omisego/omgshop/pages/transaction/generate/GenerateTransactionActivity.kt
@@ -0,0 +1,13 @@
+package co.omisego.omgshop.pages.transaction.generate
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import co.omisego.omgshop.R
+
+class GenerateTransactionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_transaction)
+    }
+}
